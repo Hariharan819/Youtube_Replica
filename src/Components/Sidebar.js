e8bcaaa04f9e6d88bec438e8fb1a6c57020f5b3c
Hariharan <hariharanvenkatachalam11@gmail.com>
2024-09-16 13:18:11 +0530

    side bar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { homeicon, Subscriptionsicon, ytshortsicon} from "../Utilis/Constant";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div>
+      <ul>
+        <li className="flex"><img className="h-5" src={homeicon} alt="" /> Home</li>
+        <li className="flex"> <img className="h-5" src={ytshortsicon} alt="" />Shorts</li>
+        <li className="flex" > <img className="h-5" src={Subscriptionsicon} alt="" />Subscriptions</li>
+      </ul>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/Utilis/Constant.js
+++ src/Utilis/Constant.js
@@ -4,3 +4,6 @@ export const youtube_logo =
   "https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg";
 export const usericon ="https://static.vecteezy.com/system/resources/previews/019/879/186/large_2x/user-icon-on-transparent-background-free-png.png";
 export const searchicon="https://cdn3.iconfinder.com/data/icons/feather-5/24/search-512.png";
+export const homeicon="https://img.icons8.com/?size=100&id=84005&format=png&color=000000";
+export const ytshortsicon="https://img.icons8.com/?size=100&id=2X8KoO8YIB6T&format=png&color=000000";
+export const Subscriptionsicon="https://img.icons8.com/?size=100&id=RNvQnmjWm0Wh&format=png&color=000000";
