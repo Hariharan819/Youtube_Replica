d2f3d3ef8c0962579a33f9cd2955b66fce67e2c0
Hariharan <hariharanvenkatachalam11@gmail.com>
2024-09-18 12:01:19 +0530

    sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -18,75 +18,80 @@ import { useSelector } from "react-redux";
 const Sidebar = () => {
   const togglevent = useSelector((store) => store.app.togglevent);
   return !togglevent ? null : (
-    <div className="w-40 -my-1 shadow-md cursor-pointer ">
+    <div className="w-40 -my-1 shadow-md cursor-pointer px-1 ">
       <ul className="">
-        <li className="flex px-2 py-3">
+        <li className="flex px-2 py-3 hover:bg-slate-500 hover:rounded-sm h-12">
           <img className="h-5" src={homeicon} alt="" /> Home
         </li>
-        <li className="flex px-2">
-          {" "}
+        <li className="flex px-2 hover:bg-slate-500 hover:rounded-sm h-12 items-center  ">
           <img className="h-5" src={ytshortsicon} alt="" />
           Shorts
         </li>
-        <li className="flex px-2 py-3">
-          {" "}
-          <img className="h-5" src={Subscriptionsicon} alt="" />
+        <li className="flex px-2  hover:bg-slate-500 hover:rounded-sm h-12  items-center">
+          <img
+            className="h-5 hover:bg-slate-500 hover:rounded-sm "
+            src={Subscriptionsicon}
+            alt=""
+          />
           Subscriptions
         </li>
       </ul>
-      <h4 className="py-3 px-1 font-semibold flex ">
+      <hr />
+      <h4 className=" px-1 font-semibold flex  hover:bg-slate-500 hover:rounded-sm h-12  items-center ">
         <img className="h-5" src={Subscriptionsicon} alt="" />
         Subscriptions
       </h4>
-      <ul className="px-2">
-        <li className="">Music</li>
-        <li className="py-3">Sports</li>
-        <li>Gaming</li>
-        <li className="py-3">Movies</li>
-      </ul>
-      <h2 className=" px-1 py-3 font-semibold">Explore</h2>
-      <ul>
-        <li className="flex pt-2 px-2 py-3">
-          {" "}
-          <img className="h-5" src={trendingicon} alt="" /> Trending
+      <ul className="">
+        <li className="hover:bg-slate-500 hover:rounded-sm h-12 items-center flex px-2">
+          Music
         </li>
-        <li className="flex pt-2 px-2 py-3">
-          {" "}
+        <li className=" hover:bg-slate-500 hover:rounded-sm h-12  items-center flex px-2">
+          Sports
+        </li>
+        <li className="hover:bg-slate-500 hover:rounded-sm h-12  items-center flex px-2">
+          Gaming
+        </li>
+        <li className=" hover:bg-slate-500 hover:rounded-sm  h-12  items-center flex px-2">
+          Movies
+        </li>
+      </ul>
+      <hr />
+      <h2 className=" px-1  font-semibold  hover:bg-slate-500 hover:rounded-sm h-12  items-center flex ">
+        Explore
+      </h2>
+      <ul>
+        <li className="flex  px-2 hover:bg-slate-500 hover:rounded-sm h-12  items-center ">
+          <img className="h-5 " src={trendingicon} alt="" /> Trending
+        </li>
+        <li className="flex  px-2  hover:bg-slate-500 hover:rounded-sm h-12  items-center ">
           <img className="h-5" src={musicincon} alt="" />
           Music
         </li>
-        <li className="flex pt-2 px-1 py-3">
-          {" "}
+        <li className="flex px-1  hover:bg-slate-500 hover:rounded-sm  items-center h-12">
           <img className="h-5 px-1" src={shoppingicon} alt="" />
           Shopping
         </li>
-        <li className="flex pt-2 px-1 py-3">
-          {" "}
+        <li className="flex  px-1 hover:bg-slate-500 hover:rounded-sm h-12  items-center ">
           <img className="h-5 px-1" src={gamingicon} alt="" />
           Gaming
         </li>
-        <li className="flex pt-2 px-2 py-3">
-          {" "}
+        <li className="flex  px-2   hover:bg-slate-500 hover:rounded-sm  items-center h-12 ">
           <img className="h-5" src={sportsicon} alt="" />
           Sports
         </li>
-        <li className="flex pt-2 px-1 py-3">
-          {" "}
+        <li className="flex px-1  hover:bg-slate-500 hover:rounded-sm  items-center h-12 ">
           <img className="h-5 px-1" src={liveicon} alt="" />
           Live
         </li>
-        <li className="flex pt-2 px-1 py-3">
-          {" "}
+        <li className="flex px-1  hover:bg-slate-500 hover:rounded-sm   items-center h-12">
           <img className="h-5 px-1" src={courseicon} alt="" />
           Courses
         </li>
-        <li className="flex pt-2 px-1 py-3">
-          {" "}
+        <li className="flex  px-1 hover:bg-slate-500 hover:rounded-sm   items-center h-12 ">
           <img className="h-5 px-1" src={newsicon} alt="" />
           News
         </li>
-        <li className="flex pt-2 px-1 py-3">
-          {" "}
+        <li className="flex  px-1 hover:bg-slate-500 hover:rounded-sm h-12  items-center ">
           <img className="h-5 px-1" src={podcasticon} alt="" />
           Podcasts
         </li>
