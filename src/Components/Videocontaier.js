a71464070193e154e2c837b1c591df9c6127b753
Hariharan <hariharanvenkatachalam11@gmail.com>
2024-10-26 20:45:03 +0530

    Debouncing

--- src/Components/Videocontaier.js
+++ src/Components/Videocontaier.js
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import { youtubevideo_api } from "../Utilis/Constant";
 import Videocards from "./Videocards";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Videocontainer = () => {
-
   const [videodata, setvideodata] = useState([]);
   useEffect(() => {
-    getvideos();
+    const timer = setTimeout(() => {
+      getvideos();
+    }, 200);
+    return () => {
+      clearTimeout(timer);
+    };
   }, []);
   const getvideos = async () => {
     const api_data = await fetch(youtubevideo_api);
@@ -19,7 +23,9 @@ const Videocontainer = () => {
   return (
     <div className="flex flex-wrap">
       {videodata.map((videodata) => (
-        <Link  key={videodata.id} to={"/watch?v=" + videodata.id}><Videocards data={videodata} /></Link>
+        <Link key={videodata.id} to={"/watch?v=" + videodata.id}>
+          <Videocards data={videodata} />
+        </Link>
       ))}
     </div>
   );
