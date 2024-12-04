3fa86c4baddc543db740a98eb3c267b0f4749753
Hariharan <hariharanvenkatachalam11@gmail.com>
2024-12-04 15:02:37 +0530

    Searchpage

--- src/Components/Watchpagesearch.js
+++ src/Components/Watchpagesearch.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect} from "react";
+import { useLocation } from "react-router";
+import { API_OPTIONS } from "../Utilis/Constant";
+
 
 const Watchpagesearch = () => {
-  return <div></div>;
+  const location = useLocation();
+// const [queryvalue,setqueryvalue]=useState("");
+  const getQueryParam = () =>
+     {
+    const queryParams = new URLSearchParams(location.search);
+    return queryParams.get("search");
+  };
+  const query = getQueryParam();
+  console.log(query);
+
+  const getsuggestion = async () => {
+    const api_data = await fetch( "https://api.themoviedb.org/3/search/movie?query="+ {query} +"&include_adult=false&language=en-US&page=1",API_OPTIONS);
+    if(api_data){
+      const json = await api_data?.json();
+      console.log(json);
+    }
+   
+  };
+  useEffect(() => {
+   getsuggestion()
+    // eslint-disable-next-line
+    },[""]);
+  return (
+    <div className="pt-60">
+     
+    </div>
+  );
 };
 
 export default Watchpagesearch;
--- src/Utilis/Constant.js
+++ src/Utilis/Constant.js
@@ -33,17 +33,20 @@ export const newsicon =
 export const courseicon =
   "https://img.icons8.com/?size=100&id=TxBgWXMwBiPE&format=png&color=000000";
 const YOUR_API_KEY = "AIzaSyAjpk-jTTojG66A11KMAsGstN4kP5-Q6qs";
+
 export const youtubevideo_api =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&q=TamilNadu&q=Chennai&maxResults=90&key=" +
   YOUR_API_KEY;
+
 export const Suggestionapi =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
-export const API_OPTIONS = {
-  method: "GET",
+
+
+export const API_OPTIONS  = {
+  method: 'GET',
   headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Y2JhMjk1NjU1MTBiOWMyZTYyZDc5ODkyM2MyY2ZhMiIsIm5iZiI6MTcyMjQxMDEyMC4zODg4MTYsInN1YiI6IjY2YTllMzk2NGIyNjIxNTI5YWNkNWU3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LYD1bidq4FbRevZFl98OaI_ALUWzOB-HhOunED4Rsjo",
-  },
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MDU5ODllYTM3MTc2N2Y2NjM1NGQyZTkzMGUzNmYxYiIsIm5iZiI6MTczMzMwNDAwNS4zMTcsInN1YiI6IjY3NTAxZWM1OWJlZTY0NjljMTQ1NzY1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZsDxMVKWMcAHDj28Ft_lE3c8jNUyL0OyfOAfZ6AuUJs'
+  }
 };
