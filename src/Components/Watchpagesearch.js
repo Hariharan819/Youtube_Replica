1cf6473c6a1dae76a1b4436ef470c0ee9d24cf67
Hariharan <hariharanvenkatachalam11@gmail.com>
2024-12-04 17:14:17 +0530

    Searchpage

--- src/Components/Watchpagesearch.js
+++ src/Components/Watchpagesearch.js
@@ -1,36 +1,38 @@
-import React, { useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import { API_OPTIONS } from "../Utilis/Constant";
 
-
 const Watchpagesearch = () => {
   const location = useLocation();
-// const [queryvalue,setqueryvalue]=useState("");
-  const getQueryParam = () =>
-     {
+  const [queryvalue, setqueryvalue] = useState([]);
+  const getQueryParam = () => {
     const queryParams = new URLSearchParams(location.search);
     return queryParams.get("search");
   };
   const query = getQueryParam();
-  console.log(query);
+  // console.log(query);
 
   const getsuggestion = async () => {
-    const api_data = await fetch( "https://api.themoviedb.org/3/search/movie?query="+ {query} +"&include_adult=false&language=en-US&page=1",API_OPTIONS);
-    if(api_data){
+    const api_data = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        { query } +
+        "&include_adult=false&language=en-US&page=1",
+      API_OPTIONS
+    );
+    if (api_data) {
       const json = await api_data?.json();
-      console.log(json);
+      setqueryvalue(json?.results);
+      console.log(json?.results);
     }
-   
   };
+
   useEffect(() => {
-   getsuggestion()
+    getsuggestion();
     // eslint-disable-next-line
-    },[""]);
-  return (
-    <div className="pt-60">
-     
-    </div>
-  );
+  }, [""]);
+  return <div className="pt-10">
+
+  </div>;
 };
 
 export default Watchpagesearch;
